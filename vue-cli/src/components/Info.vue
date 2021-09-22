585182a63475f9c7c69a4b8b7bfd40f290ea7790
Mat780 <mateusfelipe10@live.com>
2021-09-22 13:09:47 -0400

    Curso de Vue 3: #09 Conhecendo diretivas

--- vue-cli/src/components/Info.vue
+++ vue-cli/src/components/Info.vue
@@ -1,5 +1,7 @@
 <template>
 <div>
+    <p v-if="esta_trabalhando">Estou trabalhando no momento</p>
+    <p v-else>Estou em busca de novas oportunidades!</p>
     <p>Estou codando no momento.</p>
     <p>Utilizo as seguintes tecnologias:</p>
     <ul>
@@ -9,11 +11,19 @@
         <li>HTML e CSS</li>
         <li>JavaScript</li>
     </ul>
+    <p v-if="mostrarEmail">Mande um email para: {{ email }}</p>
 </div>
 </template>
 
 <script>
 export default {
-    name: "Info"
+    name: "Info",
+    data() {
+        return{
+            esta_trabalhando: false,
+            mostrarEmail: true,
+            email: "m.felipe@ufms.br"
+        }
+    }
 }
 </script>
