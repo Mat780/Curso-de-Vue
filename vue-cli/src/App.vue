1d52ecf43a64b3cc9afba7e77d71091938488e0c
Mat780 <mateusfelipe10@live.com>
2021-09-22 08:27:43 -0400

    Curso de Vue 3: #07 LifeCycle hooks

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,14 +1,26 @@
 <template>
-  <primeiro-comp />
+<div>
+  <PrimeiroComp />
+  <p>O dado de teste é: {{ dado }}</p>
+  <LifeCycle />
+</div>
 </template>
 
 <script>
 import PrimeiroComp from './components/PrimeiroComp.vue'
+import LifeCycle from './components/LifeCycle.vue'
 
   export default{
     name: "App",
     components: {
-        PrimeiroComp
+        PrimeiroComp,
+        LifeCycle
+    },
+
+    data() {
+      return{
+        dado: "Teste"
+      }
     }
   }
 </script>
---
+++ vue-cli/src/components/LifeCycle.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1>Meu nome é: {{ name }}</h1>
+</template>
+
+<script>
+export default {
+    name: "LifeCycle",
+    data() {
+        return {
+            name: "Ainda não sei"
+        }
+    },
+    created() {
+        setTimeout(() => {
+            this.name = "Giovanna"
+        }, 1000)
+        
+    },
+    mounted() {   
+        setTimeout(() => {
+            this.name = "Matheus"
+        }, 2000);
+    },
+}
+</script>
