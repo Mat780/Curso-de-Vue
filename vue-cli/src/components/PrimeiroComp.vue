06d3bc7e27dabe1ebb3aa08952ba1b53e83e0a4b
Mat780 <mateusfelipe10@live.com>
2021-09-22 08:10:56 -0400

    Curso de Vue 3: #06 Dados em Componentes

--- vue-cli/src/components/PrimeiroComp.vue
+++ vue-cli/src/components/PrimeiroComp.vue
@@ -1,9 +1,18 @@
 <template>
-    <h1>Olá vue</h1>
+    <div>
+        <h1>Olá vue</h1>
+        <p>Meu nome é {{ name }} e estudo para ser um {{ job }}.</p>
+    </div>
 </template>
 
 <script>
     export default{
-        name: "PrimeiroComp"
+        name: "PrimeiroComp",
+        data() {
+            return{
+                name: "Matheus",
+                job: "Programador"
+            }
+        }
     }
 </script>
