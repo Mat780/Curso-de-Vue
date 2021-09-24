b3dfbfd15824fb4f06adeb880a9b4c0b7fc56ab0
Mat780 <mateusfelipe10@live.com>
2021-09-24 11:49:22 -0400

    Curso de Vue 3: #14 Eventos

--- vue-cli/src/components/Form.vue
+++ vue-cli/src/components/Form.vue
@@ -1,11 +1,14 @@
 <template>
     <div>
         <h2>Meu Formulario:</h2>
-        <form action="">
-            <div><InputForm /></div>
-            <div><InputForm /></div>
+        <form action="" @submit="enviarFormulario($event)">
+            <div><input type="text" v-model="name"></div>
+            <div><input type="text" v-model="email"></div>
             <div><Submit /></div>
         </form>
+        <p>Conteudo</p>
+        <p>Conteudo</p>
+        <p>Conteudo</p>
     </div>
 </template>
 
@@ -13,10 +16,41 @@
 import InputForm from './form/InputForm.vue'
 import Submit from './form/Submit.vue'
 export default {
+    name: "Form",
+
     components: { 
         InputForm,
         Submit
     },
-    name: "Form"
+
+    data() {
+        return {
+            name: "",
+            email: ""
+        }
+    },
+
+    methods: {
+        enviarFormulario(e){
+
+            e.preventDefault();
+
+            const name = this.name;
+            const email = this.email;
+
+            console.log("Formulário Enviado!");
+            console.log("O nome é: " + name);
+            console.log("O email é: " + email);
+
+            // Ajax para mandar para o banco de dados
+
+        }
+    },
 }
 </script>
+
+<style scoped>
+    p{
+        margin: 150px;
+    }
+</style>
