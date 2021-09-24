ddcc525f5e0e48cb83f8aec19a6725e69d9fcb81
Mat780 <mateusfelipe10@live.com>
2021-09-24 12:10:36 -0400

    Curso de Vue 3: #15 Multiplos eventos

--- vue-cli/src/components/PrimeiroComp.vue
+++ vue-cli/src/components/PrimeiroComp.vue
@@ -2,10 +2,12 @@
     <div>
         <h1>Olá vue</h1>
         <p>Meu nome é {{ name }} e estudo para ser um {{ job }}.</p>
+        <MultiplosEventos />
     </div>
 </template>
 
 <script>
+import MultiplosEventos from './MultiplosEventos.vue'
     export default{
         name: "PrimeiroComp",
         data() {
@@ -13,6 +15,10 @@
                 name: "Matheus",
                 job: "Programador"
             }
+        },
+
+        components: {
+            MultiplosEventos,
         }
     }
 </script>
