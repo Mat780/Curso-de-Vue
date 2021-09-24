2365c8a78dbf40ab8faf92827e14a151ff5940d6
Mat780 <mateusfelipe10@live.com>
2021-09-24 15:21:59 -0400

    Curso de Vue 3: #16 Reutilizacao de Componentes

--- vue-cli/src/components/Picture.vue
+++ vue-cli/src/components/Picture.vue
@@ -1,15 +1,24 @@
 <template>
-    <img :src="avatar" :alt="descricao">
+    <div>
+        <img :src="avatar" :alt="descricao">
+        <Reutilazacao />
+    </div>
 </template>
 
 <script>
+import Reutilazacao from './Reutilazacao.vue'
 export default {
     name: 'Picture',
+    components: { 
+        Reutilazacao
+    },
+    
     data() {
         return{
             avatar: "/img/avatar.png",
             descricao: "Avatarzin"
         }
-    }
+    },
+    
 }
 </script>
--- vue-cli/src/components/PrimeiroComp.vue
+++ vue-cli/src/components/PrimeiroComp.vue
@@ -3,11 +3,13 @@
         <h1>Olá vue</h1>
         <p>Meu nome é {{ name }} e estudo para ser um {{ job }}.</p>
         <MultiplosEventos />
+        <Reutilizacao />
     </div>
 </template>
 
 <script>
 import MultiplosEventos from './MultiplosEventos.vue'
+import Reutilizacao from './Reutilazacao.vue'
     export default{
         name: "PrimeiroComp",
         data() {
@@ -19,6 +21,7 @@ import MultiplosEventos from './MultiplosEventos.vue'
 
         components: {
             MultiplosEventos,
+            Reutilizacao,
         }
     }
 </script>
