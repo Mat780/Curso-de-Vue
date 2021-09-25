68c491d4de1a3ae97f127384b4a3506e4d79318e
Mat780 <mateusfelipe10@live.com>
2021-09-24 21:40:44 -0400

    Curso de Vue 3: #18 Emit

--- vue-cli/src/components/Picture.vue
+++ vue-cli/src/components/Picture.vue
@@ -1,22 +1,30 @@
 <template>
     <div>
         <img :src="avatar" :alt="descricao">
-        <Reutilazacao />
+        <MudarImg @mudarImagem="trocarImagem"/>
     </div>
 </template>
 
 <script>
+import MudarImg from './MudarImg.vue'
 import Reutilazacao from './Reutilazacao.vue'
 export default {
     name: 'Picture',
     components: { 
-        Reutilazacao
+        Reutilazacao,
+        MudarImg
     },
     
     data() {
         return{
             avatar: "/img/avatar.png",
             descricao: "Avatarzin"
+        }
+    },
+
+    methods: {
+        trocarImagem(){
+            this.avatar = "/img/avatar2.png"
         }
     },
     
